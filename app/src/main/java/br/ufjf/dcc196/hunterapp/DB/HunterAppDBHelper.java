1cd2a512039591e1bad97d8c9dd86a501243525b
Jônatas <jjsfandre@gmail.com>
2019-07-10 19:59:27 -0300

    Incluindo método para inserção de Categorias default

--- app/src/main/java/br/ufjf/dcc196/hunterapp/DB/HunterAppDBHelper.java
+++ app/src/main/java/br/ufjf/dcc196/hunterapp/DB/HunterAppDBHelper.java
@@ -1,5 +1,6 @@
 package br.ufjf.dcc196.hunterapp.DB;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -7,7 +8,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 import br.ufjf.dcc196.hunterapp.DB.HunterAppContract;
 
 public class HunterAppDBHelper extends SQLiteOpenHelper {
-    public static final int DATABASE_VERSION=1;
+    public static final int DATABASE_VERSION=2;
     public static final String DATABASE_NAME="ToDoList";
 
     public HunterAppDBHelper(Context context){
@@ -20,7 +21,7 @@ public class HunterAppDBHelper extends SQLiteOpenHelper {
         db.execSQL(HunterAppContract.Producao.CREATE_TABLE);
         db.execSQL(HunterAppContract.Categoria.CREATE_TABLE);
         db.execSQL(HunterAppContract.Atividade.CREATE_TABLE);
-        //addBaseData(db);
+        addBaseData(db);
     }
 
     @Override
@@ -30,6 +31,21 @@ public class HunterAppDBHelper extends SQLiteOpenHelper {
         db.execSQL(HunterAppContract.Categoria.DROP_TABLE);
         db.execSQL(HunterAppContract.Atividade.DROP_TABLE);
         onCreate(db);
+    }
+
+    private void addBaseData(SQLiteDatabase db){
+        addCategoriaData(db);
+    }
+
+    private void addCategoriaData(SQLiteDatabase db){
+
+        ContentValues values = new ContentValues();
+        values.put(HunterAppContract.Categoria.COLUMN_TITULO,"Categoria 1");
+        db.insert(HunterAppContract.Categoria.TABLE_NAME,null,values);
+        values.put(HunterAppContract.Categoria.COLUMN_TITULO,"Categoria 2");
+        db.insert(HunterAppContract.Categoria.TABLE_NAME,null,values);
+        values.put(HunterAppContract.Categoria.COLUMN_TITULO,"Categoria 3");
+        db.insert(HunterAppContract.Categoria.TABLE_NAME,null,values);
     }
 
 }
