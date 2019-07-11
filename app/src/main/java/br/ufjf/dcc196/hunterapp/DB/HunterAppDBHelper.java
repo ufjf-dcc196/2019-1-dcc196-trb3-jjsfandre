dab60dcc2ac5becdc2491de9c7fd5c15207d5a25
Jônatas André <jjsfandre@gmail.com>
2019-07-11 19:47:39 -0300

    Adaptando método de inserção de dados básicos

--- app/src/main/java/br/ufjf/dcc196/hunterapp/DB/HunterAppDBHelper.java
+++ app/src/main/java/br/ufjf/dcc196/hunterapp/DB/HunterAppDBHelper.java
@@ -51,29 +51,21 @@ public class HunterAppDBHelper extends SQLiteOpenHelper {
         Categoria c3 = new Categoria();
         c3.setTitulo("Categoria 3");
 
-        ContentValues values = populateContentValueCategoria(c1);
-        db.insert(HunterAppContract.Categoria.TABLE_NAME,null,values);
-
-        values = populateContentValueCategoria(c2);
-        db.insert(HunterAppContract.Categoria.TABLE_NAME,null,values);
-
-        values = populateContentValueCategoria(c3);
-        db.insert(HunterAppContract.Categoria.TABLE_NAME,null,values);
+        inserirCategoria(c1);
+        inserirCategoria(c2);
+        inserirCategoria(c3);
     }
 
     private void addCandidatoData(SQLiteDatabase db){
 
         Candidato c1 = new Candidato("Candidato 1", "30/03/1994","3215-0101","perfil 1","mail1@mail.com");
-        ContentValues values = populateContentValueCandidato(c1);
-        db.insert(HunterAppContract.Candidato.TABLE_NAME,null,values);
-
         Candidato c2 = new Candidato("Candidato 2", "30/03/1996","3215-0202","perfil 2","mail2@mail.com");
-        values = populateContentValueCandidato(c2);
-        db.insert(HunterAppContract.Candidato.TABLE_NAME,null,values);
-
         Candidato c3 = new Candidato("Candidato 3", "30/03/1998","3215-0303","perfil 3","mail3@mail.com");
-        values = populateContentValueCandidato(c3);
-        db.insert(HunterAppContract.Candidato.TABLE_NAME,null,values);
+
+        inserirCandidato(c1);
+        inserirCandidato(c2);
+        inserirCandidato(c3);
+        
     }
 
     //endregion
