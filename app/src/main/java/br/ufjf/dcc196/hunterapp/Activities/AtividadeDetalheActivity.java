485d22f98f4718eac99d1db7ca4065a7a7b7fdad
Jônatas André <jjsfandre@gmail.com>
2019-07-14 19:14:18 -0300

    Concluindo implementação do Crud de Atividades

--- app/src/main/java/br/ufjf/dcc196/hunterapp/Activities/AtividadeDetalheActivity.java
+++ app/src/main/java/br/ufjf/dcc196/hunterapp/Activities/AtividadeDetalheActivity.java
@@ -18,9 +18,50 @@ import br.ufjf.dcc196.hunterapp.Model.*;
 
 public class AtividadeDetalheActivity extends AppCompatActivity {
 
+    private EditText descricao;
+    private EditText data;
+    private EditText horas;
+    public Long idAtividade;
+    public Long idProducao;
+
+    public Atividade atividade;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_atividade_detalhe);
+
+        descricao = findViewById(R.id.editDescricaoAtividadeDetalhe);
+        data = findViewById(R.id.editDataAtividadeDetalhe);
+        horas = findViewById(R.id.editHorasAtividadeDetalhe);
+
+        idAtividade = getIntent().getLongExtra("idAtividade",0);
+        HunterAppDBHelper dbHelper = new HunterAppDBHelper(getApplicationContext());
+        idProducao = getIntent().getLongExtra("idProducao",0);
+
+        atividade = dbHelper.getAtividadeById(idAtividade);
+
+        descricao.setText(atividade.getDescricao());
+        data.setText(atividade.getData());
+        horas.setText(atividade.getHoras().toString());
+
+        Button btnSaveDetalheAtividade = findViewById(R.id.btnSaveDetalheAtividade);
+
+        btnSaveDetalheAtividade.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                HunterAppDBHelper dbHelper = new HunterAppDBHelper(getApplicationContext());
+                atividade.setDescricao(descricao.getText().toString());
+                atividade.setData(data.getText().toString());
+                atividade.setHoras(Double.parseDouble(horas.getText().toString()));
+
+                dbHelper.atualizarAtividade(atividade);
+
+                Intent resultado = new Intent();
+
+                setResult(RESULT_OK, resultado);
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/br/ufjf/dcc196/hunterapp/Activities/AtividadeNovoActivity.java
+++ app/src/main/java/br/ufjf/dcc196/hunterapp/Activities/AtividadeNovoActivity.java
@@ -18,9 +18,48 @@ import br.ufjf.dcc196.hunterapp.Model.*;
 
 public class AtividadeNovoActivity extends AppCompatActivity {
 
+    private EditText descricao;
+    private EditText data;
+    private EditText horas;
+    public Long idAtividade;
+    public Long idProducao;
+
+    public Atividade atividade;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_atividade_novo);
+
+        descricao = findViewById(R.id.editDescricaoAtividadeNovo);
+        data = findViewById(R.id.editDataAtividadeNovo);
+        horas = findViewById(R.id.editHorasAtividadeNovo);
+
+        idAtividade = getIntent().getLongExtra("idAtividade",0);
+        HunterAppDBHelper dbHelper = new HunterAppDBHelper(getApplicationContext());
+        idProducao = getIntent().getLongExtra("idProducao",0);
+
+
+
+        Button btnSaveNovoAtividade = findViewById(R.id.btnSaveNovoAtividade);
+
+        btnSaveNovoAtividade.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                HunterAppDBHelper dbHelper = new HunterAppDBHelper(getApplicationContext());
+                atividade = new Atividade();
+                atividade.setDescricao(descricao.getText().toString());
+                atividade.setData(data.getText().toString());
+                atividade.setHoras(Double.parseDouble(horas.getText().toString()));
+                atividade.setProducaoId(idProducao);
+
+                dbHelper.inserirAtividade(atividade);
+
+                Intent resultado = new Intent();
+
+                setResult(RESULT_OK, resultado);
+                finish();
+            }
+        });
     }
 }
